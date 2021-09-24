c1273cca2ce11ce516a9100497e6465099fa0173
Aleksey Danilov <de.tempore@gmail.com>
2021-09-24 12:03:25 +0300

    Настройка pug js, обработка многостраничных шаблоновб отвалился stylus

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,26 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const PATHS = {
+  src: path.resolve(__dirname, 'src/'),
+  pages: path.resolve(__dirname, 'src/pages'),
+}
+const PAGES = fs.readdirSync(`${PATHS.pages}/`).filter(fileName => fileName.endsWith('.pug'));
+
 module.exports = {
   mode: "development",
-  entry:  './src/index.js',
+  entry:  './src/scripts/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.pug'
-    }),
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PATHS.pages}/${page}`,
+      filename: `./${page.replace(/\.pug/, '.html')}`
+    })),
     new MiniCssExtractPlugin({
       filename: 'style.css'
     }),
@@ -21,6 +29,15 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  resolve: {
+    alias: {
+      '@' : path.resolve(__dirname, 'src'),
+      '@blocks' : path.resolve(__dirname, 'src/blocks'),
+      '@pages' : path.resolve(__dirname, 'src/pages'),
+      '@scripts' : path.resolve(__dirname, 'src/scripts'),
+      '@styles' : path.resolve(__dirname, 'src/styles')
+    }
   },
   module: {
     rules: [
