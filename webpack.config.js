be0ad745e2e9ecda6143e1d1242898b121acac98
Aleksey Danilov <de.tempore@gmail.com>
2021-10-01 17:18:43 +0300

    Копирование ассетов в дист. Настройки сохранения путей изображений после обработки CSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,20 @@ const path = require('path');
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const PATHS = {
   src: path.resolve(__dirname, 'src/'),
+  dist: path.resolve(__dirname, 'dist/'),
   pages: path.resolve(__dirname, 'src/pages'),
+  scripts: path.resolve(__dirname, 'src/scripts'),
+  assets: path.resolve(__dirname, 'src/assets'),
 }
 const PAGES = fs.readdirSync(`${PATHS.pages}/`).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
   mode: "development",
-  entry:  './src/scripts/index.js',
+  entry:  `${PATHS.scripts}/index.js`, //'./src/scripts/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
@@ -28,11 +32,16 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'style.css'
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: PATHS.assets, to: `${PATHS.dist}/assets` },
+      ],
+    }),
   ],
   output: {
     filename: 'script.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    clean: true
   },
   resolve: {
     alias: {
@@ -40,7 +49,8 @@ module.exports = {
       '@blocks' : path.resolve(__dirname, 'src/blocks'),
       '@pages' : path.resolve(__dirname, 'src/pages'),
       '@scripts' : path.resolve(__dirname, 'src/scripts'),
-      '@styles' : path.resolve(__dirname, 'src/styles')
+      '@styles' : path.resolve(__dirname, 'src/styles'),
+      '@assets' : path.resolve(__dirname, 'src/assets')
     }
   },
   module: {
@@ -57,7 +67,18 @@ module.exports = {
       },
       {
         test: /\.styl$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"], 
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          {loader: "css-loader"},
+          {
+            loader: "stylus-loader",
+            options: {
+              stylusOptions:{
+                resolveURL: true,
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.pug$/,
@@ -66,7 +87,14 @@ module.exports = {
             loader: 'simple-pug-loader'
           }
         ]
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext][query]'
+        }
+      },
     ]
   }
 };
