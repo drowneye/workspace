20e27fedc08769f9433191605f9f477854255bf6
Aleksey D <87472619+drowneye@users.noreply.github.com>
2021-10-01 23:44:22 +0300

    PUG & Structure (#4)
    * Настройка pug js, обработка многостраничных шаблоновб отвалился stylus
    
    * Стуктурирования SRC директории. Базовая разметка
    
    * Настройка stylus. Подключение блоков. Начальная организация структуры проекта
    
    * Копирование ассетов в дист. Настройки сохранения путей изображений после обработки CSS
    
    * Маленькая правочка: корректировка путей в webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,58 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const PATHS = {
+  src: path.resolve(__dirname, 'src'),
+  dist: path.resolve(__dirname, 'dist/'),
+  pages: path.resolve(__dirname, 'src/pages'),
+  scripts: path.resolve(__dirname, 'src/scripts'),
+  assets: path.resolve(__dirname, 'src/assets'),
+}
+const PAGES = fs.readdirSync(`${PATHS.pages}/`).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
+  context: PATHS.src,
   mode: "development",
-  entry:  './src/index.js',
+  entry:  './scripts/index.js', //'./src/scripts/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.pug'
-    }),
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PATHS.pages}/${page}`,
+      filename: `./${page.replace(/\.pug/, '.html')}`,
+      minify: {
+        collapseWhitespace: false,
+        removeComments: true,
+      }
+    })),
     new MiniCssExtractPlugin({
       filename: 'style.css'
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: PATHS.assets, to: `${PATHS.dist}/assets` },
+      ],
+    }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'script.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    clean: true
+  },
+  resolve: {
+    alias: {
+      '@' : path.resolve(__dirname, 'src'),
+      '@blocks' : path.resolve(__dirname, 'src/blocks'),
+      '@pages' : path.resolve(__dirname, 'src/pages'),
+      '@scripts' : path.resolve(__dirname, 'src/scripts'),
+      '@styles' : path.resolve(__dirname, 'src/styles'),
+      '@assets' : path.resolve(__dirname, 'src/assets')
+    }
   },
   module: {
     rules: [
@@ -35,12 +67,19 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
         test: /\.styl$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"], 
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          {loader: "css-loader"},
+          {
+            loader: "stylus-loader",
+            options: {
+              stylusOptions:{
+                resolveURL: true,
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.pug$/,
@@ -49,7 +88,14 @@ module.exports = {
             loader: 'simple-pug-loader'
           }
         ]
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext][query]'
+        }
+      },
     ]
   }
 };
